ff2cbe2b2bb4dccc846a67d8c18e9cb3a2e6bfaa
anweisen <anweisen@web.de>
2021-07-18 15:12:20 +0200

    Add methods to send queries

--- driver/src/main/java/net/anweisen/cloud/driver/network/SocketChannel.java
+++ driver/src/main/java/net/anweisen/cloud/driver/network/SocketChannel.java
@@ -1,15 +1,28 @@
 package net.anweisen.cloud.driver.network;
 
+import net.anweisen.cloud.driver.network.handler.SocketChannelHandler;
+import net.anweisen.cloud.driver.network.packet.Packet;
 import net.anweisen.cloud.driver.network.packet.PacketSender;
+import net.anweisen.utilities.common.concurrent.task.Task;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.UUID;
 
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
  */
 public interface SocketChannel extends PacketSender {
+
+	@Nonnull
+	Task<Packet> registerQueryResponseHandler(@Nonnull UUID uniqueId);
+
+	@Nonnull
+	Task<Packet> sendQueryAsync(@Nonnull Packet packet);
+
+	@Nullable
+	Packet sendQuery(@Nonnull Packet packet);
 
 	void close() throws Exception;
 
