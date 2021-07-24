ece94078dfc566aea650c8d1aed71e3bf57ba40f
anweisen <anweisen@web.de>
2021-07-24 14:05:04 +0200

    Use ClassLoader of driver

--- driver/src/main/java/net/anweisen/cloud/driver/console/HeaderPrinter.java
+++ driver/src/main/java/net/anweisen/cloud/driver/console/HeaderPrinter.java
@@ -1,5 +1,7 @@
 package net.anweisen.cloud.driver.console;
 
+import net.anweisen.cloud.driver.CloudDriver;
+
 import javax.annotation.Nonnull;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,8 +18,8 @@ public final class HeaderPrinter {
 
 	private HeaderPrinter() {}
 
-	public static void printHeader(@Nonnull Console console) {
-		InputStream stream = HeaderPrinter.class.getClassLoader().getResourceAsStream("header.txt");
+	public static void printHeader(@Nonnull Console console, @Nonnull CloudDriver driver) {
+		InputStream stream = driver.getClass().getClassLoader().getResourceAsStream("header.txt");
 		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(stream), StandardCharsets.UTF_8))) {
 			String input;
 			while ((input = bufferedReader.readLine()) != null) {
