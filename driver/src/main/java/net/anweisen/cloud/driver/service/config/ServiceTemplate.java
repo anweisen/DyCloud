62c399c414fd51788d14d1983af5f5f50b819538
anweisen <anweisen@web.de>
2021-07-25 12:29:00 +0200

    Create ServiceTemplate object

---
+++ driver/src/main/java/net/anweisen/cloud/driver/service/config/ServiceTemplate.java
@@ -0,0 +1,58 @@
+package net.anweisen.cloud.driver.service.config;
+
+import net.anweisen.cloud.driver.CloudDriver;
+import net.anweisen.cloud.driver.network.packet.protocol.Buffer;
+import net.anweisen.cloud.driver.network.packet.protocol.SerializableObject;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public class ServiceTemplate implements SerializableObject {
+
+	private String storage;
+	private String name;
+
+	public ServiceTemplate(@Nonnull String storage, @Nonnull String name) {
+		this.storage = storage;
+		this.name = name;
+	}
+
+	public ServiceTemplate() {
+	}
+
+	@Override
+	public void write(@Nonnull Buffer buffer) {
+		buffer.writeString(storage);
+		buffer.writeString(name);
+	}
+
+	@Override
+	public void read(@Nonnull Buffer buffer) {
+		storage = buffer.readString();
+		name = buffer.readString();
+	}
+
+	@Nonnull
+	public String getStorage() {
+		return storage;
+	}
+
+	@Nonnull
+	public String getName() {
+		return name;
+	}
+
+	@Nullable
+	public TemplateStorage findStorage() {
+		return CloudDriver.getInstance().getServiceConfigManager().getTemplateStorage(storage);
+	}
+
+	@Override
+	public String toString() {
+		return "ServiceTemplate[" + storage + "/" + name + "]";
+	}
+}
