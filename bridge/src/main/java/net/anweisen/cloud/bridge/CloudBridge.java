c6b433e8de7726d11dd5a7864c9577c670327925
anweisen <anweisen@web.de>
2021-08-07 13:05:30 +0200

    Start bridge module

---
+++ bridge/src/main/java/net/anweisen/cloud/bridge/CloudBridge.java
@@ -0,0 +1,11 @@
+package net.anweisen.cloud.bridge;
+
+import net.anweisen.cloud.base.module.CloudModule;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public final class CloudBridge extends CloudModule {
+
+}
---
+++ bridge/src/main/java/net/anweisen/cloud/bridge/proxy/ProxyCloudBridge.java
@@ -0,0 +1,12 @@
+package net.anweisen.cloud.bridge.proxy;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public class ProxyCloudBridge extends Plugin {
+
+
+}
