f11428766dc6a30ef03aa074bb85687d2d2e0837
anweisen <anweisen@web.de>
2021-08-08 02:19:55 +0200

    Add launcher for node

---
+++ node/src/main/java/net/anweisen/cloud/node/Launcher.java
@@ -0,0 +1,33 @@
+package net.anweisen.cloud.node;
+
+import net.anweisen.cloud.driver.console.Console;
+import net.anweisen.cloud.driver.console.ConsoleLogger;
+import net.anweisen.cloud.driver.console.JLine3Console;
+import net.anweisen.utilities.common.logging.ILogger;
+import net.anweisen.utilities.common.logging.LogLevel;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public final class Launcher {
+
+	public static void main(String[] args) throws Exception {
+		Console console = new JLine3Console();
+		ILogger logger = new ConsoleLogger(console);
+		init(console, logger);
+
+		CloudNode cloud = new CloudNode(logger, console);
+		cloud.start();
+	}
+
+	private static void init(@Nonnull Console console, @Nonnull ILogger logger) {
+		ILogger.setConstantFactory(logger);
+
+		System.setOut(logger.asPrintStream(LogLevel.INFO));
+		System.setErr(logger.asPrintStream(LogLevel.ERROR));
+	}
+
+}
