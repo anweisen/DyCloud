a1ffc44595c478ad2a500f48d0ba0f52cc3a82f7
anweisen <anweisen@web.de>
2021-08-17 11:47:33 +0200

    Update packet channel comments

--- driver/src/main/java/net/anweisen/cloud/driver/network/packet/PacketConstants.java
+++ driver/src/main/java/net/anweisen/cloud/driver/network/packet/PacketConstants.java
@@ -10,9 +10,9 @@ public final class PacketConstants {
 	public static final int PUBLISH_CONFIG_CHANNEL = 2;             // master   -> node
 	public static final int DATABASE_CHANNEL = 3;                   // node    <-> master  <-> wrapper
 	public static final int REQUEST_API_CHANNEL = 4;                // master  <-> node
-	public static final int SERVICE_INFO_PUBLISH_CHANNEL = 5;       // master  <-> node
+	public static final int SERVICE_INFO_PUBLISH_CHANNEL = 5;       // node    <-> master   -> wrapper
 	public static final int SERVICE_UPDATE_SELF_INFO_CHANNEL = 6;   // wrapper  -> master
-	public static final int PLAYER_API_CHANNEL = 7;                 // node    <-> master  <-> wrapper
+	public static final int PLAYER_API_CHANNEL = 7;                 // node    <-  master  <-> wrapper
 
 	private PacketConstants() {}
 
