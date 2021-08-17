c9eb05ba62094318c2176f04e18791071b2297e3
anweisen <anweisen@web.de>
2021-08-17 12:18:18 +0200

    Improve ServiceInfo

--- driver/src/main/java/net/anweisen/cloud/driver/service/specific/ServiceInfo.java
+++ driver/src/main/java/net/anweisen/cloud/driver/service/specific/ServiceInfo.java
@@ -2,10 +2,12 @@ package net.anweisen.cloud.driver.service.specific;
 
 import com.google.common.base.Preconditions;
 import net.anweisen.cloud.driver.CloudDriver;
+import net.anweisen.cloud.driver.network.HostAndPort;
 import net.anweisen.cloud.driver.network.packet.protocol.Buffer;
 import net.anweisen.cloud.driver.network.packet.protocol.SerializableObject;
 import net.anweisen.cloud.driver.node.NodeInfo;
 import net.anweisen.cloud.driver.service.config.ServiceTask;
+import net.anweisen.utilities.common.config.Document;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
@@ -25,22 +27,34 @@ public final class ServiceInfo implements SerializableObject {
 	private String taskName;
 	private int serviceNumber;
 
-	private String nodeName;
+	private ServiceEnvironment environment;
 	private ServiceState state;
+	private boolean ready;
+
+	private String nodeName;
+	private String nodeAddress;
 
 	private int port;
+	private boolean staticService;
 
-	public ServiceInfo() {
+	private Document properties;
+
+	private ServiceInfo() {
 	}
 
-	public ServiceInfo(@Nonnull UUID uniqueId, @Nullable String dockerContainerId, @Nonnull String taskName, @Nonnegative int serviceNumber, @Nonnull String nodeName, int port, @Nonnull ServiceState state) {
+	public ServiceInfo(@Nonnull UUID uniqueId, @Nullable String dockerContainerId, @Nonnull String taskName, @Nonnegative int serviceNumber, @Nonnull ServiceEnvironment environment,
+	                   @Nonnull ServiceState state, @Nonnull String nodeName, @Nonnull String nodeAddress, int port, boolean staticService, @Nonnull Document properties) {
 		this.uniqueId = uniqueId;
 		this.dockerContainerId = dockerContainerId;
 		this.taskName = taskName;
 		this.serviceNumber = serviceNumber;
-		this.nodeName = nodeName;
+		this.environment = environment;
 		this.state = state;
+		this.nodeName = nodeName;
+		this.nodeAddress = nodeAddress;
 		this.port = port;
+		this.staticService = staticService;
+		this.properties = properties;
 	}
 
 	@Override
@@ -49,8 +63,13 @@ public final class ServiceInfo implements SerializableObject {
 		buffer.writeString(taskName);
 		buffer.writeInt(serviceNumber);
 		buffer.writeString(nodeName);
+		buffer.writeString(nodeAddress);
 		buffer.writeEnumConstant(state);
+		buffer.writeBoolean(ready);
+		buffer.writeEnumConstant(environment);
 		buffer.writeInt(port);
+		buffer.writeBoolean(staticService);
+		buffer.writeDocument(properties);
 	}
 
 	@Override
@@ -59,13 +78,18 @@ public final class ServiceInfo implements SerializableObject {
 		taskName = buffer.readString();
 		serviceNumber = buffer.readInt();
 		nodeName = buffer.readString();
+		nodeAddress = buffer.readString();
 		state = buffer.readEnumConstant(ServiceState.class);
+		ready = buffer.readBoolean();
+		environment = buffer.readEnumConstant(ServiceEnvironment.class);
 		port = buffer.readInt();
+		staticService = buffer.readBoolean();
+		properties = buffer.readDocument();
 	}
 
 	@Nonnull
 	public String getName() {
-		return this.taskName + "-" + this.serviceNumber;
+		return taskName + "-" + serviceNumber;
 	}
 
 	@Nonnull
@@ -94,12 +118,40 @@ public final class ServiceInfo implements SerializableObject {
 	}
 
 	@Nonnull
+	public String getNodeAddress() {
+		return nodeAddress;
+	}
+
+	@Nonnull
 	public ServiceState getState() {
 		return state;
 	}
 
+	public boolean isReady() {
+		return state == ServiceState.RUNNING && ready;
+	}
+
+	@Nonnull
+	public ServiceEnvironment getEnvironment() {
+		return environment;
+	}
+
 	public int getPort() {
 		return port;
+	}
+
+	public boolean isStatic() {
+		return staticService;
+	}
+
+	@Nonnull
+	public Document getProperties() {
+		return properties;
+	}
+
+	@Nonnull
+	public HostAndPort getAddress() {
+		return new HostAndPort(nodeAddress, port);
 	}
 
 	@Nullable
@@ -118,13 +170,18 @@ public final class ServiceInfo implements SerializableObject {
 	}
 
 	public void setState(@Nonnull ServiceState state) {
-		Preconditions.checkNotNull(state, "The given state is not null");
+		Preconditions.checkNotNull(state, "The given state cannot be null");
 		this.state = state;
+	}
+
+	public void setReady() {
+		Preconditions.checkArgument(state == ServiceState.RUNNING, "Cannot set service ready when not in State.RUNNING");
+		this.ready = true;
 	}
 
 	@Override
 	public String toString() {
-		return "Service[" + getName() + ":" + uniqueId + " containerId=" + dockerContainerId + " node=" + nodeName + " port=" + port + " state=" + state + "]";
+		return "Service[name=" + getName() + " node=" + nodeName + " port=" + port + " static=" + staticService + " state=" + state + ":" + (state == ServiceState.RUNNING ? (ready ? "ready" : "unready") : "") + "]";
 	}
 
 	@Override
@@ -132,11 +189,22 @@ public final class ServiceInfo implements SerializableObject {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		ServiceInfo that = (ServiceInfo) o;
-		return serviceNumber == that.serviceNumber && Objects.equals(uniqueId, that.uniqueId) && Objects.equals(dockerContainerId, that.dockerContainerId) && Objects.equals(taskName, that.taskName) && Objects.equals(nodeName, that.nodeName) && state == that.state;
+		return serviceNumber == that.serviceNumber
+			&& ready == that.ready
+			&& port == that.port
+			&& staticService == that.staticService
+			&& environment == that.environment
+			&& state == that.state
+			&& Objects.equals(uniqueId, that.uniqueId)
+			&& Objects.equals(dockerContainerId, that.dockerContainerId)
+			&& Objects.equals(taskName, that.taskName)
+			&& Objects.equals(nodeName, that.nodeName)
+			&& Objects.equals(nodeAddress, that.nodeAddress)
+			&& Objects.equals(properties, that.properties);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(uniqueId, dockerContainerId, taskName, serviceNumber, nodeName, state);
+		return Objects.hash(uniqueId, dockerContainerId, taskName, serviceNumber, environment, state, ready, nodeName, nodeAddress, port, staticService, properties);
 	}
 }
