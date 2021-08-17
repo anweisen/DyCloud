ef34a03a8d94d55b91b96986af182fd1366dea11
anweisen <anweisen@web.de>
2021-08-17 11:57:08 +0200

    Add getters for managers

--- driver/src/main/java/net/anweisen/cloud/driver/CloudDriver.java
+++ driver/src/main/java/net/anweisen/cloud/driver/CloudDriver.java
@@ -2,6 +2,16 @@ package net.anweisen.cloud.driver;
 
 import com.google.common.base.Preconditions;
 import net.anweisen.cloud.driver.database.DatabaseManager;
+import net.anweisen.cloud.driver.event.EventManager;
+import net.anweisen.cloud.driver.event.defaults.DefaultEventManager;
+import net.anweisen.cloud.driver.network.SocketComponent;
+import net.anweisen.cloud.driver.node.NodeManager;
+import net.anweisen.cloud.driver.player.PlayerManager;
+import net.anweisen.cloud.driver.player.permission.PermissionManager;
+import net.anweisen.cloud.driver.service.ServiceFactory;
+import net.anweisen.cloud.driver.service.ServiceManager;
+import net.anweisen.cloud.driver.service.config.ServiceConfigManager;
+import net.anweisen.utilities.common.collection.NamedThreadFactory;
 import net.anweisen.utilities.common.function.ExceptionallyRunnable;
 import net.anweisen.utilities.common.logging.ILogger;
 import net.anweisen.utilities.common.misc.FileUtils;
@@ -18,12 +28,14 @@ import java.util.concurrent.ScheduledExecutorService;
  */
 public abstract class CloudDriver {
 
-	protected final ScheduledExecutorService executor = Executors.newScheduledThreadPool(4);
+	protected final ScheduledExecutorService executor = Executors.newScheduledThreadPool(4, new NamedThreadFactory("CloudTask"));
 	protected final EventManager eventManager = new DefaultEventManager();
 
 	protected final ILogger logger;
 	protected final DriverEnvironment environment;
 	protected final Path tempDirectory;
+
+	protected PermissionManager permissionManager;
 
 	{
 		Runtime.getRuntime().addShutdownHook(new Thread((ExceptionallyRunnable) this::shutdown, "ShutdownHook"));
@@ -74,7 +86,34 @@ public abstract class CloudDriver {
 	}
 
 	@Nonnull
+	public abstract SocketComponent getSocketComponent();
+
+	@Nonnull
 	public abstract DatabaseManager getDatabaseManager();
+
+	@Nonnull
+	public abstract ServiceConfigManager getServiceConfigManager();
+
+	@Nonnull
+	public abstract ServiceFactory getServiceFactory();
+
+	@Nonnull
+	public abstract ServiceManager getServiceManager();
+
+	@Nonnull
+	public abstract NodeManager getNodeManager();
+
+	@Nonnull
+	public abstract PlayerManager getPlayerManager();
+
+	@Nonnull
+	public PermissionManager getPermissionManager() {
+		Preconditions.checkNotNull(permissionManager, "No Permission System available");
+		return permissionManager;
+	}
+
+	@Nonnull
+	public abstract String getComponentName();
 
 	private static CloudDriver instance;
 
