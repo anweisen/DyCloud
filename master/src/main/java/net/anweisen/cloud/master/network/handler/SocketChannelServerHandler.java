fdcd425ec87c47ba1a397aedbcef9c94b74cbc7e
anweisen <anweisen@web.de>
2021-08-17 11:53:48 +0200

    Add serverside channel closelogic

--- master/src/main/java/net/anweisen/cloud/master/network/handler/SocketChannelServerHandler.java
+++ master/src/main/java/net/anweisen/cloud/master/network/handler/SocketChannelServerHandler.java
@@ -2,15 +2,19 @@ package net.anweisen.cloud.master.network.handler;
 
 import net.anweisen.cloud.driver.CloudDriver;
 import net.anweisen.cloud.driver.event.network.PacketReceiveEvent;
+import net.anweisen.cloud.driver.event.network.SocketChannelCloseEvent;
 import net.anweisen.cloud.driver.event.network.SocketChannelConnectEvent;
 import net.anweisen.cloud.driver.network.InternalQueryResponseManager;
 import net.anweisen.cloud.driver.network.SocketChannel;
 import net.anweisen.cloud.driver.network.handler.SocketChannelHandler;
 import net.anweisen.cloud.driver.network.packet.Packet;
+import net.anweisen.cloud.driver.network.packet.def.ServiceInfoPublishPacket.PublishType;
 import net.anweisen.cloud.master.CloudMaster;
 import net.anweisen.cloud.master.node.NodeServer;
+import net.anweisen.cloud.master.service.specific.CloudService;
 
 import javax.annotation.Nonnull;
+import java.util.stream.Collectors;
 
 /**
  * @author anweisen | https://github.com/anweisen
@@ -58,14 +62,27 @@ public class SocketChannelServerHandler implements SocketChannelHandler {
 	public void handleChannelClose(@Nonnull SocketChannel channel) throws Exception {
 		CloudMaster cloud = CloudMaster.getInstance();
 
-		cloud.getEventManager().callEvent(new SocketChannelConnectEvent(channel));
+		cloud.getEventManager().callEvent(new SocketChannelCloseEvent(channel));
 		cloud.getLogger().info("Channel[client={} server={}] was closed", channel.getClientAddress(), channel.getServerAddress());
+
+		CloudService service = cloud.getServiceManager().getServiceByChannel(channel);
+		if (service != null) {
+			cloud.getLogger().info("Service '{}' has disconnected", service.getInfo().getName());
+			cloud.publishUpdate(PublishType.DISCONNECTED, service.getInfo());
+			cloud.getServiceManager().handleServiceUpdate(PublishType.DISCONNECTED, service.getInfo());
+			return;
+		}
 
 		NodeServer nodeServer = cloud.getNodeManager().getNodeServer(channel);
 		if (nodeServer != null) {
 			cloud.getLogger().warn("Node '{}' has disconnected", nodeServer.getInfo().getName());
 			cloud.getNodeManager().getNodeServers().remove(nodeServer);
+			return;
 		}
+
+		cloud.getLogger().warn("Channel[client={} server={}] was neither a node or service", channel.getClientAddress(), channel.getServerAddress());
+		cloud.getLogger().extended("Nodes: {}", cloud.getNodeManager().getNodeServers().stream().map(current -> current.getInfo().getName() + " | " + current.getChannel()).collect(Collectors.joining("   ")));
+		cloud.getLogger().extended("Services: {}", cloud.getServiceManager().getServices().stream().map(current -> current.getInfo().getName() + " | " + current.getChannel()).collect(Collectors.joining("   ")));
 	}
 
 	protected boolean inWhitelist(@Nonnull SocketChannel channel) {
