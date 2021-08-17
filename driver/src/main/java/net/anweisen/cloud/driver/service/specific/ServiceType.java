c4afa7189cf1c07f2278976b401174ca47780add
anweisen <anweisen@web.de>
2021-08-18 00:34:43 +0200

    Clean code

--- driver/src/main/java/net/anweisen/cloud/driver/service/specific/ServiceType.java
+++ driver/src/main/java/net/anweisen/cloud/driver/service/specific/ServiceType.java
@@ -1,13 +1,19 @@
 package net.anweisen.cloud.driver.service.specific;
 
+import net.anweisen.cloud.driver.service.config.ServiceTask;
+
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
+ *
+ * @see ServiceEnvironment
+ *
+ * @see ServiceTask#getEnvironment()
+ * @see ServiceInfo#getEnvironment()
  */
 public enum ServiceType {
 
 	SERVER(30000),
-
 	PROXY(25565);
 
 	private final int startPort;
