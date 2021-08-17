d1a1295dd054445f76aa941241ceb38b0a6a62cb
anweisen <anweisen@web.de>
2021-08-17 12:18:36 +0200

    Add ServiceProperties constants

---
+++ driver/src/main/java/net/anweisen/cloud/driver/service/specific/ServiceProperties.java
@@ -0,0 +1,18 @@
+package net.anweisen.cloud.driver.service.specific;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+@SuppressWarnings("GrazieInspection")
+public final class ServiceProperties {
+
+	public static final String ONLINE_COUNT = "online";         // SERVER   PROXY
+	public static final String MAX_PLAYER_COUNT = "max";        // SERVER   PROXY
+	public static final String PLAYERS = "players";             // SERVER   PROXY
+	public static final String PLUGINS = "plugins";             // SERVER   PROXY
+	public static final String MESSAGE_CHANNELS = "channels";   //          PROXY
+
+	private ServiceProperties() {}
+
+}
