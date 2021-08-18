d2bf97f427fb2bbd9e5f74cf390e745b2976bb2f
anweisen <anweisen@web.de>
2021-08-18 04:06:23 +0200

    Add perms module

---
+++ modules/perms/src/main/java/net/anweisen/cloud/modules/perms/CloudPermsModule.java
@@ -0,0 +1,26 @@
+package net.anweisen.cloud.modules.perms;
+
+import net.anweisen.cloud.base.module.CloudModule;
+import net.anweisen.cloud.driver.DriverEnvironment;
+import net.anweisen.cloud.driver.player.permission.impl.CloudPermissionManager;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public final class CloudPermsModule extends CloudModule {
+
+	@Override
+	protected void onLoad() {
+		if (getDriver().getEnvironment() != DriverEnvironment.MASTER) return;
+
+		if (!getConfig().contains("enabled")) {
+			getConfig().set("enabled", true).save();
+		}
+		getLogger().debug("CloudPermsModule Status: enabled={}", getConfig().getBoolean("enabled"));
+		if (getConfig().getBoolean("enabled")) {
+			getDriver().setPermissionManager(new CloudPermissionManager());
+		}
+	}
+
+}
