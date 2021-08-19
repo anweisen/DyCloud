f1889a6230c64946f61cab6e3a98406d994361ad
anweisen <anweisen@web.de>
2021-08-19 14:53:20 +0200

    Add getProxyConnection & getServer

--- driver/src/main/java/net/anweisen/cloud/driver/player/CloudPlayer.java
+++ driver/src/main/java/net/anweisen/cloud/driver/player/CloudPlayer.java
@@ -1,8 +1,11 @@
 package net.anweisen.cloud.driver.player;
 
 import net.anweisen.cloud.driver.network.HostAndPort;
+import net.anweisen.cloud.driver.player.data.PlayerNetworkProxyConnection;
+import net.anweisen.cloud.driver.service.specific.ServiceInfo;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * @author anweisen | https://github.com/anweisen
@@ -15,6 +18,14 @@ public interface CloudPlayer extends CloudOfflinePlayer {
 
 	@Nonnull
 	HostAndPort getAddress();
+
+	@Nonnull
+	PlayerNetworkProxyConnection getProxyConnection();
+
+	@Nullable
+	ServiceInfo getServer();
+
+	void setServer(@Nullable ServiceInfo server);
 
 	boolean isOnline();
 
