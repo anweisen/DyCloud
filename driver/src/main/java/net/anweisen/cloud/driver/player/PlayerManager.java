748e742b5ad67465e8771771370cedef8bdec8f6
anweisen <anweisen@web.de>
2021-08-19 14:53:53 +0200

    Add more getter

--- driver/src/main/java/net/anweisen/cloud/driver/player/PlayerManager.java
+++ driver/src/main/java/net/anweisen/cloud/driver/player/PlayerManager.java
@@ -11,14 +11,23 @@ import java.util.UUID;
  */
 public interface PlayerManager {
 
+	int getOnlinePlayerCount();
+
 	@Nonnull
 	Collection<CloudPlayer> getOnlinePlayers();
+
+	int getOnlineTaskPlayerCount(@Nonnull String taskName);
+
+	@Nonnull
+	Collection<CloudPlayer> getOnlineTaskPlayers(@Nonnull String taskName);
 
 	@Nullable
 	CloudPlayer getOnlinePlayerByName(@Nonnull String playerName);
 
 	@Nullable
 	CloudPlayer getOnlinePlayerByUniqueId(@Nonnull UUID uniqueId);
+
+	long getRegisteredPlayerCount();
 
 	@Nonnull
 	Collection<CloudOfflinePlayer> getOfflinePlayers();
