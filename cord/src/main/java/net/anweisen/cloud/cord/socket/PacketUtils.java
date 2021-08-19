f289355aa74c10fe86e364e3bf64a405bc6b666e
anweisen <anweisen@web.de>
2021-08-19 17:43:42 +0200

    Add PacketUtils for minecraft

---
+++ cord/src/main/java/net/anweisen/cloud/cord/socket/PacketUtils.java
@@ -0,0 +1,19 @@
+package net.anweisen.cloud.cord.socket;
+
+import io.netty.util.AttributeKey;
+import net.anweisen.cloud.cord.socket.stream.ProxyDownstreamHandler;
+import net.anweisen.cloud.cord.socket.stream.ProxyUpstreamHandler;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public class PacketUtils {
+
+	public static final AttributeKey<ConnectionState> CONNECTION_STATE = AttributeKey.valueOf("connectionstate");
+	public static final AttributeKey<ProxyUpstreamHandler> UPSTREAM_HANDLER = AttributeKey.valueOf("upstreamhandler");
+	public static final AttributeKey<ProxyDownstreamHandler> DOWNSTREAM_HANDLER = AttributeKey.valueOf("downstreamhandler");
+
+
+
+}
