68048a6a4cb0e8714f7eabec22d51834463ea15c
anweisen <anweisen@web.de>
2021-08-21 14:34:11 +0200

    Make class final & constructor private

--- cord/src/main/java/net/anweisen/cloud/cord/socket/PacketUtils.java
+++ cord/src/main/java/net/anweisen/cloud/cord/socket/PacketUtils.java
@@ -8,12 +8,12 @@ import net.anweisen.cloud.cord.socket.stream.ProxyUpstreamHandler;
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
  */
-public class PacketUtils {
+public final class PacketUtils {
 
 	public static final AttributeKey<ConnectionState> CONNECTION_STATE = AttributeKey.valueOf("connectionstate");
 	public static final AttributeKey<ProxyUpstreamHandler> UPSTREAM_HANDLER = AttributeKey.valueOf("upstreamhandler");
 	public static final AttributeKey<ProxyDownstreamHandler> DOWNSTREAM_HANDLER = AttributeKey.valueOf("downstreamhandler");
 
-
+	private PacketUtils() {}
 
 }
