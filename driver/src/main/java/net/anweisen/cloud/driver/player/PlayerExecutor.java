491a415caca8bd7d816fca3517cd42f0e6112b2b
anweisen <anweisen@web.de>
2021-08-25 18:51:02 +0200

    Add all methods to PlayerExecutor

--- driver/src/main/java/net/anweisen/cloud/driver/player/PlayerExecutor.java
+++ driver/src/main/java/net/anweisen/cloud/driver/player/PlayerExecutor.java
@@ -1,6 +1,7 @@
 package net.anweisen.cloud.driver.player;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.UUID;
 
 /**
@@ -9,7 +10,69 @@ import java.util.UUID;
  */
 public interface PlayerExecutor {
 
+	/**
+	 * @return the uuid of the player this executor is managing
+	 */
 	@Nonnull
 	UUID getPlayerUniqueId();
+
+	/**
+	 * Sends a message to the player.
+	 *
+	 * @param message the message
+	 */
+	default void sendMessage(@Nonnull String message) {
+		sendMessages(message);
+	}
+
+	/**
+	 * Sends messages to the player.
+	 *
+	 * @param messages the message lines to send
+	 */
+	default void sendMessages(@Nonnull String... messages) {
+		sendConditionalMessage(null, messages);
+	}
+
+	/**
+	 * Sends messages to the player if the player has the given permission.
+	 * The permission check will be done on the proxy the player is on.
+	 *
+	 * @param permission the permission the player must have
+	 * @param messages the message lines to send
+	 */
+	void sendConditionalMessage(@Nullable String permission, @Nonnull String... messages);
+
+	/**
+	 * Sends an action to the player.
+	 *
+	 * @param message the message to send
+	 */
+	void sendActionbar(@Nonnull String message);
+
+	/**
+	 * Sends a title to the player.
+	 *
+	 * @param title the first title line
+	 * @param subtitle the second title line
+	 * @param fadeIn the time the title will fade in given in ticks
+	 * @param stay the time the title will stay on the screen after fading in before fading out given in ticks
+	 * @param fadeOut the time the title will fade out after the stay time expired
+	 */
+	void sendTitle(@Nonnull String title, @Nonnull String subtitle, int fadeIn, int stay, int fadeOut);
+
+	/**
+	 * Sends the player to the given server.
+	 *
+	 * @param serverName the target server's name
+	 */
+	void connect(@Nonnull String serverName);
+
+	/**
+	 * Disconnects the player from the proxy.
+	 *
+	 * @param kickReason the reason with which the player should be kicked
+	 */
+	void disconnect(@Nullable String kickReason);
 
 }
