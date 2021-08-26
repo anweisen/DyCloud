7d568875dd500be170520b3d89f11d2c52def4ea
anweisen <anweisen@web.de>
2021-08-26 19:30:33 +0200

    Print exception too

--- driver/src/main/java/net/anweisen/cloud/driver/network/netty/NettyChannelHandler.java
+++ driver/src/main/java/net/anweisen/cloud/driver/network/netty/NettyChannelHandler.java
@@ -46,7 +46,7 @@ public abstract class NettyChannelHandler extends SimpleChannelInboundHandler<Pa
 
 	@Override
 	public void exceptionCaught(@Nonnull ChannelHandlerContext context, @Nonnull Throwable ex) throws Exception {
-		trace("Error on channel {}: {}", channel, ex.getClass().getName());
+		trace("Error on channel {}: {}", channel, ex.getClass().getName(), ex.getMessage());
 		if (!(ex instanceof IOException)) {
 			ex.printStackTrace();
 		}
