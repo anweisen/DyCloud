1bebef3921bd541fae3559eb0f973a9e1fb74d4c
anweisen <anweisen@web.de>
2021-08-26 18:20:56 +0200

    Improve config publishing

--- driver/src/main/java/net/anweisen/cloud/driver/network/listener/PublishConfigListener.java
+++ driver/src/main/java/net/anweisen/cloud/driver/network/listener/PublishConfigListener.java
@@ -5,47 +5,76 @@ import net.anweisen.cloud.driver.console.LoggingApiUser;
 import net.anweisen.cloud.driver.network.SocketChannel;
 import net.anweisen.cloud.driver.network.packet.Packet;
 import net.anweisen.cloud.driver.network.packet.PacketListener;
+import net.anweisen.cloud.driver.network.packet.def.ConfigInitPacket;
+import net.anweisen.cloud.driver.network.packet.def.ConfigInitPacket.PropertySection;
 import net.anweisen.cloud.driver.network.packet.protocol.Buffer;
+import net.anweisen.cloud.driver.player.defaults.DefaultCloudPlayer;
+import net.anweisen.cloud.driver.player.permission.impl.DefaultPermissionGroup;
 import net.anweisen.cloud.driver.service.config.RemoteTemplateStorage;
 import net.anweisen.cloud.driver.service.config.ServiceTask;
-import net.anweisen.cloud.driver.service.config.TemplateStorage;
+import net.anweisen.cloud.driver.service.specific.ServiceInfo;
+import net.anweisen.cloud.driver.service.specific.ServiceType;
+import net.anweisen.utilities.common.collection.ArrayWalker;
 import net.anweisen.utilities.common.logging.LogLevel;
 
 import javax.annotation.Nonnull;
-import java.util.Collection;
-import java.util.stream.Collectors;
 
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
+ *
+ * @see ConfigInitPacket#create()
  */
 public class PublishConfigListener implements PacketListener, LoggingApiUser {
 
 	@Override
 	public void handlePacket(@Nonnull SocketChannel channel, @Nonnull Packet packet) throws Exception {
-		CloudDriver cloud = CloudDriver.getInstance();
-
 		info("Received published configs from master..");
+		readNext(channel, packet.getBuffer());
+	}
 
-		Buffer buffer = packet.getBuffer();
+	private void readNext(@Nonnull SocketChannel channel, @Nonnull Buffer buffer) {
+		if (buffer.readableBytes() < 1) return;
 
-		getTargetLogger().setMinLevel(buffer.readEnumConstant(LogLevel.class));
+		PropertySection section = buffer.readEnumConstant(PropertySection.class);
+		read(section, channel, buffer);
+		readNext(channel, buffer);
+	}
 
-		Collection<ServiceTask> tasks = buffer.readObjectCollection(ServiceTask.class);
-		cloud.getServiceConfigManager().setServiceTasks(tasks);
-
-		Collection<String> templateStorageNames = buffer.readStringCollection();
-		Collection<TemplateStorage> templateStorages = templateStorageNames.stream().map(name -> new RemoteTemplateStorage(name, channel)).collect(Collectors.toList());
-		cloud.getServiceConfigManager().setTemplateStorages(templateStorages);
-
-		extended("LogLevel: " + getTargetLogger().getMinLevel());
-		extended("ServiceTasks ({}):", tasks.size());
-		for (ServiceTask task : tasks)
-			extended("- {}", task);
-		extended("TemplateStorages ({}):", templateStorageNames.size());
-		for (String templateStorage : templateStorageNames)
-			extended("- {}", templateStorage);
-
+	private void read(@Nonnull PropertySection section, @Nonnull SocketChannel channel, @Nonnull Buffer buffer) {
+		CloudDriver cloud = CloudDriver.getInstance();
+		switch (section) {
+			case LOG_LEVEL: {
+				cloud.getLogger().setMinLevel(buffer.readEnumConstant(LogLevel.class));
+				break;
+			}
+			case TASKS: {
+				cloud.getServiceConfigManager().setServiceTasks(buffer.readObjectCollection(ServiceTask.class));
+				break;
+			}
+			case SERVICES: {
+				cloud.getServiceManager().setServiceInfos(buffer.readObjectCollection(ServiceInfo.class));
+				break;
+			}
+			case START_PORTS: {
+				ArrayWalker.walk(ServiceType.values()).forEach(type -> type.setStartPort(buffer.readVarInt()));
+				break;
+			}
+			case TEMPLATE_STORAGES: {
+				for (String name : buffer.readStringCollection()) {
+					cloud.getServiceConfigManager().registerTemplateStorage(new RemoteTemplateStorage(name, channel));
+				}
+				break;
+			}
+			case ONLINE_PLAYERS: {
+				cloud.getPlayerManager().setOnlinePlayerCache(buffer.readObjectCollection(DefaultCloudPlayer.class));
+				break;
+			}
+			case PERMISSION_GROUPS: {
+				cloud.getPermissionManager().setGroupsCache(buffer.readObjectCollection(DefaultPermissionGroup.class));
+				break;
+			}
+		}
 	}
 
 }
--- driver/src/main/java/net/anweisen/cloud/driver/network/packet/def/ConfigInitPacket.java
+++ driver/src/main/java/net/anweisen/cloud/driver/network/packet/def/ConfigInitPacket.java
@@ -4,29 +4,60 @@ import net.anweisen.cloud.driver.CloudDriver;
 import net.anweisen.cloud.driver.network.packet.Packet;
 import net.anweisen.cloud.driver.network.packet.PacketConstants;
 import net.anweisen.cloud.driver.network.packet.protocol.Buffer;
-import net.anweisen.cloud.driver.service.config.TemplateStorage;
+import net.anweisen.cloud.driver.network.packet.protocol.SerializableObject;
+import net.anweisen.cloud.driver.service.specific.ServiceType;
+import net.anweisen.utilities.common.collection.ArrayWalker;
 
 import javax.annotation.Nonnull;
-import java.util.stream.Collectors;
+import java.util.Collection;
+import java.util.function.Consumer;
 
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
+ *
+ * @see net.anweisen.cloud.driver.network.listener.PublishConfigListener
  */
 public class ConfigInitPacket extends Packet {
 
 	public static ConfigInitPacket create() {
-		CloudDriver driver = CloudDriver.getInstance();
-		return new ConfigInitPacket(
-			Buffer.create()
-				.writeEnumConstant(driver.getLogger().getMinLevel())
-				.writeObjectCollection(driver.getServiceConfigManager().getTasks())
-				.writeStringCollection(driver.getServiceConfigManager().getTemplateStorages().stream().map(TemplateStorage::getName).collect(Collectors.toList()))
-		);
+		return new ConfigInitPacket();
 	}
 
-	private ConfigInitPacket(@Nonnull Buffer buffer) {
-		super(PacketConstants.PUBLISH_CONFIG_CHANNEL, buffer);
+	@SuppressWarnings("unchecked")
+	private ConfigInitPacket() {
+		super(PacketConstants.PUBLISH_CONFIG_CHANNEL, Buffer.create());
+
+		CloudDriver driver = CloudDriver.getInstance();
+
+		append(PropertySection.LOG_LEVEL, buffer -> buffer.writeEnumConstant(driver.getLogger().getMinLevel()));
+		append(PropertySection.TASKS, buffer -> buffer.writeObjectCollection(driver.getServiceConfigManager().getTasks()));
+		append(PropertySection.TEMPLATE_STORAGES, buffer -> buffer.writeStringCollection(driver.getServiceConfigManager().getTemplateStorageNames()));
+		append(PropertySection.SERVICES, buffer -> buffer.writeObjectCollection(driver.getServiceManager().getServiceInfos()));
+		append(PropertySection.START_PORTS, buffer -> ArrayWalker.walk(ServiceType.values()).forEach(type -> buffer.writeVarInt(type.getStartPort())));
+		append(PropertySection.PERMISSION_GROUPS, driver.hasPermissionManager(), buffer -> buffer.writeObjectCollection((Collection<? extends SerializableObject>) (Collection<?>) driver.getPermissionManager().getGroups()));
+		append(PropertySection.ONLINE_PLAYERS, buffer -> buffer.writeObjectCollection((Collection<? extends SerializableObject>) (Collection<?>) driver.getPlayerManager().getOnlinePlayers()));
+
+	}
+
+	private void append(@Nonnull PropertySection section, boolean condition, @Nonnull Consumer<? super Buffer> modifier) {
+		if (condition)
+			append(section, modifier);
+	}
+
+	private void append(@Nonnull PropertySection section, @Nonnull Consumer<? super Buffer> modifier) {
+		body.writeEnumConstant(section);
+		modifier.accept(body);
+	}
+
+	public enum PropertySection {
+		LOG_LEVEL,
+		TASKS,
+		TEMPLATE_STORAGES,
+		SERVICES,
+		START_PORTS,
+		PERMISSION_GROUPS,
+		ONLINE_PLAYERS
 	}
 
 }
