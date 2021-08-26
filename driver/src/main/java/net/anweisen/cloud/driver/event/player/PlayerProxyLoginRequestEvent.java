8730b21ed7ffe2b0aa78a16119f86b9e7c7603cc
anweisen <anweisen@web.de>
2021-08-26 19:11:14 +0200

    Throw is not MASTER

--- driver/src/main/java/net/anweisen/cloud/driver/event/player/PlayerProxyLoginRequestEvent.java
+++ driver/src/main/java/net/anweisen/cloud/driver/event/player/PlayerProxyLoginRequestEvent.java
@@ -1,7 +1,10 @@
 package net.anweisen.cloud.driver.event.player;
 
+import com.google.common.base.Preconditions;
+import net.anweisen.cloud.driver.CloudDriver;
+import net.anweisen.cloud.driver.DriverEnvironment;
 import net.anweisen.cloud.driver.event.Cancelable;
-import net.anweisen.cloud.driver.network.packet.def.PlayerApiPacket.PlayerActionType;
+import net.anweisen.cloud.driver.network.packet.def.PlayerEventPacket.PlayerEventType;
 import net.anweisen.cloud.driver.player.CloudPlayer;
 
 import javax.annotation.Nonnull;
@@ -10,7 +13,7 @@ import javax.annotation.Nullable;
 /**
  * Called when a player tries to connect to a proxy
  *
- * Triggered by {@link PlayerActionType#PROXY_LOGIN_REQUEST}
+ * Triggered by {@link PlayerEventType#PROXY_LOGIN_REQUEST}
  *
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
@@ -31,15 +34,20 @@ public class PlayerProxyLoginRequestEvent extends PlayerProxyEvent implements Ca
 
 	@Override
 	public void setCancelled(boolean cancelled) {
+		Preconditions.checkArgument(CloudDriver.getInstance().getEnvironment() == DriverEnvironment.MASTER, "PlayerProxyLoginRequestEvent can only be cancelled from the master");
 		this.cancelled = cancelled;
 	}
 
 	public void setCancelledReason(@Nullable String reason) {
+		Preconditions.checkArgument(CloudDriver.getInstance().getEnvironment() == DriverEnvironment.MASTER, "PlayerProxyLoginRequestEvent can only be cancelled from the master");
 		this.reason = reason;
+		if (reason != null)
+			setCancelled(true);
 	}
 
 	@Nullable
 	public String getCancelledReason() {
+		Preconditions.checkArgument(CloudDriver.getInstance().getEnvironment() == DriverEnvironment.MASTER, "PlayerProxyLoginRequestEvent can only be cancelled from the master");
 		return reason;
 	}
 }
