26a687ebcc66a9e16083b94a9fe57bec5a50b395
anweisen <anweisen@web.de>
2021-08-29 14:44:29 +0200

    Remove PUBLISH_CONFIG_CHANNEL

--- driver/src/main/java/net/anweisen/cloud/driver/network/packet/PacketConstants.java
+++ driver/src/main/java/net/anweisen/cloud/driver/network/packet/PacketConstants.java
@@ -13,18 +13,17 @@ public final class PacketConstants {
 
 	public static final int RESPONSE_CHANNEL = -1;
 	public static final int AUTH_CHANNEL = 1;                       // master  <-| node, wrapper, cord
-	public static final int PUBLISH_CONFIG_CHANNEL = 2;             // master   -> node
-	public static final int DATABASE_CHANNEL = 3;                   // master  <-| node, wrapper, cord
-	public static final int REQUEST_API_CHANNEL = 4;                // master  <-> node
-	public static final int NODE_DATA_CYCLE = 5;                    // master  <- node
-	public static final int SERVICE_INFO_PUBLISH_CHANNEL = 6;       // node     -> master                -> node, wrapper, cord
-	public static final int SERVICE_UPDATE_SELF_INFO_CHANNEL = 7;   // wrapper  -> master
-	public static final int SERVICE_CONTROL_CHANNEL = 8;            // node    <- master                <- wrapper, node cord
-	public static final int PLAYER_EVENT_CHANNEL = 9;               // wrapper  -> master
-	public static final int PLAYER_UPDATE_REMOTE_CHANNEL = 10;      // master   -> node, wrapper, cord
-	public static final int PLAYER_EXECUTOR_CHANNEL = 11;           // wrapper <- master                <- wrapper, node, cord
-	public static final int PLAYER_REMOTE_MANAGER_CHANNEL = 12;     // master  <-| wrapper, node, cord
-	public static final int CORD_CHANNEL = 13;                      // cord     -> master                -> wrapper
+	public static final int DATABASE_CHANNEL = 2;                   // master  <-| node, wrapper, cord
+	public static final int REQUEST_API_CHANNEL = 3;                // master  <-> node
+	public static final int NODE_DATA_CYCLE = 4;                    // master  <- node
+	public static final int SERVICE_INFO_PUBLISH_CHANNEL = 5;       // node     -> master                -> node, wrapper, cord
+	public static final int SERVICE_UPDATE_SELF_INFO_CHANNEL = 6;   // wrapper  -> master
+	public static final int SERVICE_CONTROL_CHANNEL = 7;            // node    <- master                <- wrapper, node cord
+	public static final int PLAYER_EVENT_CHANNEL = 8;               // wrapper  -> master
+	public static final int PLAYER_UPDATE_REMOTE_CHANNEL = 9;       // master   -> node, wrapper, cord
+	public static final int PLAYER_EXECUTOR_CHANNEL = 10;           // wrapper <- master                <- wrapper, node, cord
+	public static final int PLAYER_REMOTE_MANAGER_CHANNEL = 11;     // master  <-| wrapper, node, cord
+	public static final int CORD_CHANNEL = 12;                      // cord     -> master                -> wrapper
 
 	private static final Map<Integer, String> channelNames = new LinkedHashMap<>();
 	static {
