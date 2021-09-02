fb45070361b0331bc7f1df0c099c6e2f7af4634e
anweisen <anweisen@web.de>
2021-09-03 01:14:30 +0200

    Add control method sees

--- base/src/main/java/net/anweisen/cloud/base/module/config/ModuleState.java
+++ base/src/main/java/net/anweisen/cloud/base/module/config/ModuleState.java
@@ -1,17 +1,31 @@
 package net.anweisen.cloud.base.module.config;
 
+import net.anweisen.cloud.base.module.ModuleController;
+
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
  */
 public enum ModuleState {
 
+	/**
+	 * @see ModuleController#loadModule()
+	 */
 	LOADED,
 
+	/**
+	 * @see ModuleController#enableModule()
+	 */
 	ENABLED,
 
+	/**
+	 * @see ModuleController#disableModule()
+	 */
 	DISABLED,
 
+	/**
+	 * @see ModuleController#unregisterModule()
+	 */
 	UNREGISTERED
 
 }
