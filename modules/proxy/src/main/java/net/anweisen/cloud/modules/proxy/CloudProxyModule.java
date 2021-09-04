ad7ee3347642e2e3d01fe8c6d1954de7fe55dcdb
anweisen <anweisen@web.de>
2021-09-05 00:59:17 +0200

    Improve default config

--- modules/proxy/src/main/java/net/anweisen/cloud/modules/proxy/CloudProxyModule.java
+++ modules/proxy/src/main/java/net/anweisen/cloud/modules/proxy/CloudProxyModule.java
@@ -36,7 +36,7 @@ public final class CloudProxyModule extends CloudModule {
 					Collections.singletonList(new ProxyTabListEntryConfig(
 						Arrays.asList(
 							"§7                                                              ",
-							"§e§lMinecraftCloud §8» §7Dockerize your network",
+							"§e§lMinecraftCloud §8⏹ §7Dockerize your network",
 							"§8§l► §a{players.online} §7/ §c{players.max} §8§l┃ §7Server §8» §e{service} §8§l◄",
 							"§7"
 						),
