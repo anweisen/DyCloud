b4cd7251ded2252fef8137052b6b5b81ffdb4f65
anweisen <anweisen@web.de>
2021-09-05 00:41:16 +0200

    Create Permissions class

---
+++ driver/src/main/java/net/anweisen/cloud/driver/player/permission/Permissions.java
@@ -0,0 +1,17 @@
+package net.anweisen.cloud.driver.player.permission;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public final class Permissions {
+
+	public static final String JOIN_MAINTENANCE = "cloud.join.maintenance";
+	public static final String JOIN_FULL        = "cloud.join.full";
+	public static final String CHAT_COLORS      = "cloud.chat.color";
+	public static final String AUTO_OP          = "cloud.auto.op";
+	public static final String NOTIFY           = "cloud.notify";
+
+	private Permissions() {}
+
+}
