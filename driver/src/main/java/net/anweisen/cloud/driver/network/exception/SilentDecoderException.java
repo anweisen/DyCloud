622aabf3a1b7ea4d7b47f58bb00bd85996feff4b
anweisen <anweisen@web.de>
2021-09-07 19:31:21 +0200

    Add stacktrace

--- driver/src/main/java/net/anweisen/cloud/driver/network/exception/SilentDecoderException.java
+++ driver/src/main/java/net/anweisen/cloud/driver/network/exception/SilentDecoderException.java
@@ -1,25 +1,23 @@
 package net.anweisen.cloud.driver.network.exception;
 
-import io.netty.handler.codec.DecoderException;
-
 import javax.annotation.Nonnull;
 
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
  */
-public class SilentDecoderException extends DecoderException {
+public class SilentDecoderException extends RuntimeException {
 
-	public static final SilentDecoderException INVALID_VAR_INT = new SilentDecoderException("Invalid var int");
-	public static final SilentDecoderException BAD_PACKET_LENGTH = new SilentDecoderException("Bad packet length");
+	public static SilentDecoderException forInvalidVarInt() {
+		return new SilentDecoderException("Invalid var int");
+	}
+
+	public static SilentDecoderException forBadPacketLength() {
+		return new SilentDecoderException("Bad packet length");
+	}
 
 	private SilentDecoderException(@Nonnull String message) {
 		super(message);
-	}
-
-	@Override
-	public synchronized Throwable fillInStackTrace() {
-		return this;
 	}
 
 }
