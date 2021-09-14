1de436e176c5c41e78d537ebe29782eddd45cd55
anweisen <anweisen@web.de>
2021-09-14 21:59:49 +0200

    Also delete record when unregistered

--- modules/cloudflare/src/main/java/net/anweisen/cloud/modules/cloudflare/listener/CloudServiceStatusListener.java
+++ modules/cloudflare/src/main/java/net/anweisen/cloud/modules/cloudflare/listener/CloudServiceStatusListener.java
@@ -1,8 +1,9 @@
 package net.anweisen.cloud.modules.cloudflare.listener;
 
 import net.anweisen.cloud.driver.event.EventListener;
-import net.anweisen.cloud.driver.event.service.ServiceStartedEvent;
+import net.anweisen.cloud.driver.event.service.ServiceReadyEvent;
 import net.anweisen.cloud.driver.event.service.ServiceStoppedEvent;
+import net.anweisen.cloud.driver.event.service.ServiceUnregisteredEvent;
 import net.anweisen.cloud.modules.cloudflare.CloudCloudflareModule;
 
 import javax.annotation.Nonnull;
@@ -14,12 +15,18 @@ import javax.annotation.Nonnull;
 public class CloudServiceStatusListener {
 
 	@EventListener
-	public void onServiceStarted(@Nonnull ServiceStartedEvent event) {
+	public void onServiceReady(@Nonnull ServiceReadyEvent event) {
+		if (!event.getServiceInfo().getEnvironment().isProxy()) return;
 		CloudCloudflareModule.getInstance().createCloudflareDnsServiceEntries(event.getServiceInfo());
 	}
 
 	@EventListener
 	public void onServiceStopped(@Nonnull ServiceStoppedEvent event) {
+		CloudCloudflareModule.getInstance().getCloudflareAPI().deleteAllRecords(event.getServiceInfo().getUniqueId());
+	}
+
+	@EventListener
+	public void onServiceUnregistered(@Nonnull ServiceUnregisteredEvent event) {
 		CloudCloudflareModule.getInstance().getCloudflareAPI().deleteAllRecords(event.getServiceInfo().getUniqueId());
 	}
 
