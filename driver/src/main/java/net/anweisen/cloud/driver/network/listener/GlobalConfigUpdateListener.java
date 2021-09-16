54294048611854b0cd37c079d7dbc9846f20f3c9
anweisen <anweisen@web.de>
2021-09-16 22:56:48 +0200

    Create GlobalConfigUpdateListener

---
+++ driver/src/main/java/net/anweisen/cloud/driver/network/listener/GlobalConfigUpdateListener.java
@@ -0,0 +1,31 @@
+package net.anweisen.cloud.driver.network.listener;
+
+import net.anweisen.cloud.driver.CloudDriver;
+import net.anweisen.cloud.driver.event.global.GlobalConfigUpdatedEvent;
+import net.anweisen.cloud.driver.network.SocketChannel;
+import net.anweisen.cloud.driver.network.packet.Packet;
+import net.anweisen.cloud.driver.network.packet.PacketListener;
+import net.anweisen.cloud.driver.network.packet.def.GlobalConfigPacket.GlobalConfigPacketType;
+import net.anweisen.utilities.common.config.Document;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public class GlobalConfigUpdateListener implements PacketListener {
+
+	@Override
+	public void handlePacket(@Nonnull SocketChannel channel, @Nonnull Packet packet) throws Exception {
+		GlobalConfigPacketType packetType = packet.getBuffer().readEnumConstant(GlobalConfigPacketType.class);
+		switch (packetType) {
+			case UPDATE: {
+				Document rawData = packet.getBuffer().readDocument();
+				CloudDriver.getInstance().getGlobalConfig().setRawData(rawData);
+				CloudDriver.getInstance().getEventManager().callEvent(new GlobalConfigUpdatedEvent());
+				break;
+			}
+		}
+	}
+}
