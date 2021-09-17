a4a1b91f4181e1d96d1a8dbf404105325ca493b9
anweisen <anweisen@web.de>
2021-09-17 17:17:00 +0200

    Use new remote storage system

--- driver/src/main/java/net/anweisen/cloud/driver/service/config/RemoteTemplateStorage.java
+++ driver/src/main/java/net/anweisen/cloud/driver/service/config/RemoteTemplateStorage.java
@@ -1,12 +1,9 @@
 package net.anweisen.cloud.driver.service.config;
 
 import com.google.common.base.Preconditions;
-import net.anweisen.cloud.driver.network.SocketChannel;
-import net.anweisen.cloud.driver.network.packet.protocol.Buffer;
-import net.anweisen.cloud.driver.network.request.NetworkingApiUser;
-import net.anweisen.cloud.driver.network.request.RequestResponseType;
-import net.anweisen.cloud.driver.network.request.RequestType;
-import net.anweisen.utilities.common.collection.WrappedException;
+import net.anweisen.cloud.driver.network.NetworkingApiUser;
+import net.anweisen.cloud.driver.network.packet.def.TemplateStoragePacket;
+import net.anweisen.cloud.driver.network.packet.def.TemplateStoragePacket.TemplateStorageRequestType;
 import net.anweisen.utilities.common.concurrent.task.Task;
 
 import javax.annotation.Nonnull;
@@ -14,7 +11,6 @@ import javax.annotation.Nullable;
 import java.io.InputStream;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 
 /**
  * @author anweisen | https://github.com/anweisen
@@ -23,17 +19,9 @@ import java.util.function.Supplier;
 public class RemoteTemplateStorage implements TemplateStorage, NetworkingApiUser {
 
 	private final String name;
-	private final SocketChannel channel;
 
-	public RemoteTemplateStorage(@Nonnull String name, @Nonnull SocketChannel channel) {
+	public RemoteTemplateStorage(@Nonnull String name) {
 		this.name = name;
-		this.channel = channel;
-	}
-
-	@Nonnull
-	@Override
-	public SocketChannel getChannel() {
-		return channel;
 	}
 
 	@Nonnull
@@ -51,17 +39,14 @@ public class RemoteTemplateStorage implements TemplateStorage, NetworkingApiUser
 	@Nullable
 	@Override
 	public InputStream zipTemplate(@Nonnull ServiceTemplate template) {
-		return zipTemplateAsync(template).getOrDefault(20, TimeUnit.SECONDS, null);
+		return zipTemplateAsync(template).getOrDefault(90, TimeUnit.SECONDS, null);
 	}
 
 	@Nonnull
 	@Override
 	public Task<Collection<ServiceTemplate>> getTemplatesAsync() {
-		return sendRequest(
-			RequestType.GET_TEMPLATES,
-			buffer -> buffer.writeString(name),
-			buffer -> throwException(buffer).readObjectCollection(ServiceTemplate.class)
-		);
+		return sendPacketQueryAsync(new TemplateStoragePacket(TemplateStorageRequestType.GET_TEMPLATES, buffer -> buffer.writeString(name)))
+			.map(packet -> packet.getBuffer().readObjectCollection(ServiceTemplate.class));
 	}
 
 	@Override
@@ -70,31 +55,12 @@ public class RemoteTemplateStorage implements TemplateStorage, NetworkingApiUser
 		Preconditions.checkNotNull(template);
 		Preconditions.checkArgument(template.getStorage().equals(name), "The given ServiceTemplate must be from this TemplateStorage");
 
-		return sendChunkedRequest(
-			RequestType.LOAD_TEMPLATE_STREAM,
-			buffer -> buffer.writeString(name).writeString(template.getName())
-		).map(chunkedResponse -> {
-			RequestResponseType response = chunkedResponse.getSession().getHeader().get("response", RequestResponseType.class);
-			throwException(response, () -> Buffer.readAll(chunkedResponse.getInputStream()).readThrowable());
-			return chunkedResponse.getInputStream();
+		return sendChunkedPacketQuery(
+			new TemplateStoragePacket(TemplateStorageRequestType.LOAD_TEMPLATE_STREAM, buffer -> buffer.writeObject(template))
+		).map(response -> {
+			if (!response.getSession().getHeader().getBoolean("exists")) return null;
+			return response.getInputStream();
 		});
-	}
-
-	@Nonnull
-	protected Buffer throwException(@Nonnull Buffer buffer) {
-		throwException(buffer.readEnumConstant(RequestResponseType.class), buffer::readThrowable);
-		return buffer;
-	}
-
-	protected void throwException(@Nonnull RequestResponseType response, @Nonnull Supplier<Throwable> supplier) {
-		switch (response) {
-			case EXCEPTION:
-				Throwable ex = supplier.get();
-				if (ex == null) throw new IllegalStateException("Request returned EXCEPTION response");
-				throw new WrappedException(ex);
-			case NOT_FOUND:
-				throw new IllegalArgumentException("Remote TemplateStorage " + name + " was not found");
-		}
 	}
 
 	@Override
@@ -105,11 +71,8 @@ public class RemoteTemplateStorage implements TemplateStorage, NetworkingApiUser
 	@Nonnull
 	@Override
 	public Task<Boolean> hasTemplateAsync(@Nonnull ServiceTemplate template) {
-		return sendRequest(
-			RequestType.HAS_TEMPLATE,
-			buffer -> buffer.writeObject(template),
-			buffer -> buffer.readBoolean()
-		);
+		return sendPacketQueryAsync(new TemplateStoragePacket(TemplateStorageRequestType.HAS_TEMPLATE, buffer -> buffer.writeObject(template)))
+			.map(packet -> packet.getBuffer().readBoolean());
 	}
 
 	@Override
