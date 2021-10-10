ab4295afb1a6e15eaa00da127d8ec7c2bc98ab83
anweisen <anweisen@web.de>
2021-10-10 18:47:02 +0200

    Add shortcut

--- driver/src/main/java/net/anweisen/cloud/driver/player/chat/ChatClickEvent.java
+++ driver/src/main/java/net/anweisen/cloud/driver/player/chat/ChatClickEvent.java
@@ -1,13 +1,34 @@
 package net.anweisen.cloud.driver.player.chat;
 
+import javax.annotation.Nonnull;
+
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
  */
 public enum ChatClickEvent {
 
-	RUN_COMMAND,
-	SUGGEST_COMMAND,
-	OPEN_URL
+	RUN_COMMAND("run"),
+	SUGGEST_COMMAND("suggest"),
+	OPEN_URL("open");
 
+	private final String shortcut;
+
+	ChatClickEvent(@Nonnull String shortcut) {
+		this.shortcut = shortcut;
+	}
+
+	@Nonnull
+	public String getShortCut() {
+		return shortcut;
+	}
+
+	@Nonnull
+	public static ChatClickEvent getClickEvent(@Nonnull String shortcut) {
+		for (ChatClickEvent event : values()) {
+			if (event.getShortCut().equals(shortcut))
+				return event;
+		}
+		throw new IllegalArgumentException("Unknown ChatClickEvent for '" + shortcut + "'");
+	}
 }
