61439c14d507f21899d46789fa71c3d954a5a364
anweisen <anweisen@web.de>
2021-10-10 18:47:14 +0200

    Add helper methods

--- driver/src/main/java/net/anweisen/cloud/driver/player/chat/ChatText.java
+++ driver/src/main/java/net/anweisen/cloud/driver/player/chat/ChatText.java
@@ -14,6 +14,11 @@ import java.util.Objects;
  */
 public class ChatText implements SerializableObject {
 
+	public static final ChatText NEW_LINE = new ChatText("\n");
+	public static final ChatText SPACE = new ChatText(" ");
+	public static final ChatText EMPTY = new ChatText("");
+	public static final ChatText RESET = new ChatText("§r");
+
 	private String text;
 
 	private ChatClickEvent clickEvent;
@@ -21,11 +26,23 @@ public class ChatText implements SerializableObject {
 
 	private String hover;
 
-	private ChatText() {
+	public ChatText() {
+		this("");
 	}
 
 	public ChatText(@Nonnull String text) {
 		this.text = text;
+	}
+
+	public ChatText(@Nullable Object text) {
+		this(String.valueOf(text));
+	}
+
+	public ChatText(@Nonnull Object... text) {
+		this();
+		for (Object current : text) {
+			appendText(String.valueOf(current));
+		}
 	}
 
 	@Override
@@ -45,13 +62,13 @@ public class ChatText implements SerializableObject {
 	}
 
 	@Nonnull
-	public ChatText addHover(@Nonnull String hover) {
+	public ChatText setHover(@Nonnull String hover) {
 		this.hover = hover;
 		return this;
 	}
 
 	@Nonnull
-	public ChatText addClick(@Nonnull ChatClickEvent event, @Nonnull String value) {
+	public ChatText setClick(@Nonnull ChatClickEvent event, @Nonnull String value) {
 		this.clickEvent = event;
 		this.click = value;
 		return this;
@@ -89,9 +106,13 @@ public class ChatText implements SerializableObject {
 		return click;
 	}
 
+	public boolean isEmpty() {
+		return text.isEmpty();
+	}
+
 	@Override
 	public String toString() {
-		return "ChatText[text='" + text + "' hover='" + hover + "' click=" + clickEvent + "='" + click + "']";
+		return "ChatText[text='" + text + "' hover='" + hover + "' click=" + (clickEvent == null ? null : clickEvent + "=:'" + click + "'") + "]";
 	}
 
 	@Override
@@ -108,5 +129,14 @@ public class ChatText implements SerializableObject {
 	@Override
 	public int hashCode() {
 		return Objects.hash(text, clickEvent, click, hover);
+	}
+
+	@Nonnull
+	public static String toString(@Nonnull ChatText... text) {
+		StringBuilder builder = new StringBuilder();
+		for (ChatText current : text) {
+			builder.append(current.text);
+		}
+		return builder.toString();
 	}
 }
