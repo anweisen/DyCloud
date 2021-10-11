f81514bdcc6d0ead02f34e8f5d698276be7af9ae
anweisen <anweisen@web.de>
2021-10-11 22:15:12 +0200

    Remove SET & GET

--- driver/src/main/java/net/anweisen/cloud/driver/network/packet/def/CommandSystemPacket.java
+++ driver/src/main/java/net/anweisen/cloud/driver/network/packet/def/CommandSystemPacket.java
@@ -4,7 +4,6 @@ import net.anweisen.cloud.driver.network.packet.Packet;
 import net.anweisen.cloud.driver.network.packet.PacketConstants;
 
 import javax.annotation.Nonnull;
-import java.util.Collection;
 import java.util.UUID;
 
 /**
@@ -17,15 +16,9 @@ public class CommandSystemPacket extends Packet {
 		super(PacketConstants.COMMAND_SYSTEM_CHANNEL, newBuffer().writeEnum(payload).writeUniqueId(player).writeString(input));
 	}
 
-	public CommandSystemPacket(@Nonnull CommandSystemPayload payload, @Nonnull Collection<String> commands) {
-		super(PacketConstants.COMMAND_SYSTEM_CHANNEL, newBuffer().writeEnum(payload).writeStringCollection(commands));
-	}
-
 	public enum CommandSystemPayload {
 		EXECUTE,
-		COMPLETE,
-		SET_COMMANDS,
-		GET_COMMANDS
+		COMPLETE
 	}
 
 }
