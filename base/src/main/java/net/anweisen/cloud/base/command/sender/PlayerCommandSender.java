1e524af1c4bbef6b9ccbbf550f8c344009206eda
anweisen <anweisen@web.de>
2021-10-24 01:59:23 +0200

    Add PlayerCommandSender.of

--- base/src/main/java/net/anweisen/cloud/base/command/sender/PlayerCommandSender.java
+++ base/src/main/java/net/anweisen/cloud/base/command/sender/PlayerCommandSender.java
@@ -1,9 +1,13 @@
 package net.anweisen.cloud.base.command.sender;
 
+import com.google.common.base.Preconditions;
+import net.anweisen.cloud.base.command.sender.defaults.DefaultPlayerCommandSender;
+import net.anweisen.cloud.driver.CloudDriver;
 import net.anweisen.cloud.driver.player.CloudPlayer;
 import net.anweisen.cloud.driver.player.PlayerExecutor;
 import net.anweisen.cloud.driver.player.permission.PermissionPlayer;
 
+import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import java.util.UUID;
 
@@ -12,6 +16,18 @@ import java.util.UUID;
  * @since 1.0
  */
 public interface PlayerCommandSender extends CommandSender {
+
+	@Nonnull
+	@CheckReturnValue
+	static PlayerCommandSender of(@Nonnull UUID playerUniqueId) {
+		return of(Preconditions.checkNotNull(CloudDriver.getInstance().getPlayerManager().getOnlinePlayerByUniqueId(playerUniqueId), "Player " + playerUniqueId + " is not online"));
+	}
+
+	@Nonnull
+	@CheckReturnValue
+	static PlayerCommandSender of(@Nonnull CloudPlayer player) {
+		return new DefaultPlayerCommandSender(player);
+	}
 
 	@Nonnull
 	UUID getUniqueId();
