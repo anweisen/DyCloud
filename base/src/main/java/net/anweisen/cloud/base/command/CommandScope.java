213adb5bc3d5f256729520e0fb310c46f9526622
anweisen <anweisen@web.de>
2021-10-24 01:58:25 +0200

    Add covers(CommandSender)

--- base/src/main/java/net/anweisen/cloud/base/command/CommandScope.java
+++ base/src/main/java/net/anweisen/cloud/base/command/CommandScope.java
@@ -1,5 +1,11 @@
 package net.anweisen.cloud.base.command;
 
+import net.anweisen.cloud.base.command.sender.CommandSender;
+import net.anweisen.cloud.base.command.sender.ConsoleCommandSender;
+import net.anweisen.cloud.base.command.sender.PlayerCommandSender;
+
+import javax.annotation.Nonnull;
+
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
@@ -28,4 +34,11 @@ public enum CommandScope {
 	public boolean hasCloudPrefix() {
 		return this == CONSOLE_AND_INGAME;
 	}
+
+	public boolean covers(@Nonnull CommandSender sender) {
+		if (sender instanceof PlayerCommandSender && isIngame()) return true;
+		if (sender instanceof ConsoleCommandSender && isConsole()) return true;
+		return false;
+	}
+
 }
