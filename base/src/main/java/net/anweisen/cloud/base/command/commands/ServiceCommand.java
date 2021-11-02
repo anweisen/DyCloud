a2a66121c42f8ca7ec353ef670a420466f88cb01
anweisen <anweisen@web.de>
2021-11-02 11:32:30 +0100

    Improve list command

--- base/src/main/java/net/anweisen/cloud/base/command/commands/ServiceCommand.java
+++ base/src/main/java/net/anweisen/cloud/base/command/commands/ServiceCommand.java
@@ -6,11 +6,11 @@ import net.anweisen.cloud.base.command.annotation.CommandPath;
 import net.anweisen.cloud.base.command.sender.CommandSender;
 import net.anweisen.cloud.driver.CloudDriver;
 import net.anweisen.cloud.driver.player.permission.Permissions;
-import net.anweisen.cloud.driver.service.specific.ServiceInfo;
-import net.anweisen.cloud.driver.service.specific.ServiceProperty;
+import net.anweisen.cloud.driver.service.specific.*;
 
 import javax.annotation.Nonnull;
 import java.util.Collection;
+import java.util.function.Predicate;
 
 /**
  * @author anweisen | https://github.com/anweisen
@@ -28,12 +28,33 @@ public class ServiceCommand {
 		Collection<ServiceInfo> services = CloudDriver.getInstance().getServiceManager().getServiceInfos();
 		for (ServiceInfo service : services) {
 			sender.sendTranslation("cloud.command.service.list.entry",
-				service.getName(), service.getNodeName(), service.getState(), service.getAddress(),
+				service.getName(),
+				service.getNodeName(),
+				service.getAddress(),
+				service.getState(),
+				service.getControlState(),
 				service.isConnected() ? "Connected" : "Not Connected",
-				service.get(ServiceProperty.PHASE), service.get(ServiceProperty.ONLINE_PLAYERS)
+				service.get(ServiceProperty.PHASE),
+				service.get(ServiceProperty.ONLINE_PLAYERS),
+				service.get(ServiceProperty.MAX_PLAYERS)
 			);
 		}
-		sender.sendTranslation("cloud.command.service.list.count", CloudDriver.getInstance().getPlayerManager().getOnlinePlayerCount());
+
+		sender.sendTranslation("cloud.command.service.list.count",
+			services.size(),
+			count(services, service -> service.getState() == ServiceState.RUNNING),
+			count(services, ServiceInfo::isReady),
+			count(services, ServicePropertyHelper::isIngame),
+			count(services, service -> service.getControlState() == ServiceControlState.STARTING)
+		);
 	}
 
+	private int count(@Nonnull Collection<ServiceInfo> services, @Nonnull Predicate<ServiceInfo> filter) {
+		int number = 0;
+		for (ServiceInfo service : services) {
+			if (filter.test(service))
+				number++;
+		}
+		return number;
+	}
 }
