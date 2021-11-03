65bbf42a1e4cdcc1cfc276d5c8c0023556421368
anweisen <anweisen@web.de>
2021-11-03 14:01:14 +0100

    Optimize default command completers

--- base/src/main/java/net/anweisen/cloud/base/command/completer/ServiceTypeCompleter.java
+++ base/src/main/java/net/anweisen/cloud/base/command/completer/ServiceTypeCompleter.java
@@ -2,6 +2,7 @@ package net.anweisen.cloud.base.command.completer;
 
 import net.anweisen.cloud.base.command.sender.CommandSender;
 import net.anweisen.cloud.driver.service.specific.ServiceEnvironment;
+import net.anweisen.cloud.driver.service.specific.ServiceType;
 
 import javax.annotation.Nonnull;
 import java.util.Arrays;
@@ -12,11 +13,11 @@ import java.util.stream.Collectors;
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
  */
-public class ServiceEnvironmentCompleter implements CommandCompleter {
+public class ServiceTypeCompleter implements CommandCompleter {
 
 	@Nonnull
 	@Override
 	public Collection<String> complete(@Nonnull CommandSender sender, @Nonnull String message, @Nonnull String argument) {
-		return Arrays.stream(ServiceEnvironment.values()).map(Enum::name).collect(Collectors.toList());
+		return Arrays.stream(ServiceType.values()).map(Enum::name).collect(Collectors.toList());
 	}
 }
